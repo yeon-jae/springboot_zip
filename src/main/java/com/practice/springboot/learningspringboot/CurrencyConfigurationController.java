c392d374b8f1ff08bd4616e7e311e65edd1b7241
yeon-jae <lisanam99@naver.com>
2023-08-02 17:01:33 +0900

    springboot_log

--- src/main/java/com/practice/springboot/learningspringboot/CurrencyConfigurationController.java
+++ src/main/java/com/practice/springboot/learningspringboot/CurrencyConfigurationController.java
@@ -1,9 +1,7 @@
 package com.practice.springboot.learningspringboot;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
-import java.util.List;
 
 //http://localhost:8080/courses
 /*
@@ -17,17 +15,15 @@ import java.util.List;
 //요청에서 반환할 값
 //1. restAPI만들기-RestController주석 사용
 @RestController
-public class CourseController {
+public class CurrencyConfigurationController {
     //courses , Course: id,name,author
 //3.courses : 누군가 /courses를 입력하면 코스 목록을 반환
-    @RequestMapping("/courses")
-    public List<Course> retrieveAllCourses(){
-        return Arrays.asList(
-                new Course(1,"Learn AWS","in28minutes"),
-                new Course(2,"Learn Devops","in28minutes"),
-                new Course(3,"Learn Azure","in28minutes"),
-                new Course(4,"Learn","in28minutes")
-                );
+    @Autowired
+    private CurrencyServiceConfiguration configuration;
+
+    @RequestMapping("/currency-configuration")
+    public CurrencyServiceConfiguration retrieveAllCourses(){
+        return configuration;
     }
 
 }
