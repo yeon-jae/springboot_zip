9849f46b144e236def4be31614091923e35e9d71
yeon-jae <lisanam99@naver.com>
2023-08-03 17:16:21 +0900

    Content_PostMapping,PutMapping

--- src/main/java/com/practice/springboot/learningspringboot/controller/ContentController.java
+++ src/main/java/com/practice/springboot/learningspringboot/controller/ContentController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 /*
 {
         "productId":1,
@@ -39,14 +41,57 @@ public class ContentController {
             // 저장 도중 에러가 발생했을 때 500 Internal Server Error 상태와 함께 에러 메시지 반환
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
-   /* @GetMapping("/contents")
-    public String content(ContentDTO contentDTO){
-        System.out.println(contentDTO.getContentId());
-        System.out.println(contentDTO.getContentName());
-        System.out.println(contentDTO.getContentContent());
+    }
+    @GetMapping("/contents/{contentId}")
+        public ResponseEntity<ContentEntity> getContent(@PathVariable Long contentId) {
+            try {
+                Optional<ContentEntity> optionalContent = contentRepository.findById(contentId);
+                if (optionalContent.isPresent()) {
+                    ContentEntity contentEntity = optionalContent.get();
+                    return ResponseEntity.ok(contentEntity);
+                } else {
+                    return ResponseEntity.notFound().build();
+                }
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
+        }
 
-        return contentDTO.toString();
-    }*/
+    @DeleteMapping("/contents/{contentId}")
+    public ResponseEntity<String> deleteContent(@PathVariable Long contentId) {
+        try {
+            Optional<ContentEntity> optionalContent = contentRepository.findById(contentId);
+            if (optionalContent.isPresent()) {
+                contentRepository.deleteById(contentId);
+                return ResponseEntity.ok("콘텐츠가 삭제되었습니다.");
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("콘텐츠 삭제 중 에러가 발생했습니다.");
+        }
+    }
 
+    @PutMapping("/contents/{contentId}")
+    public ResponseEntity<ContentEntity> updateContent(@PathVariable Long contentId, @RequestBody ContentDTO contentDTO) {
+        try {
+            Optional<ContentEntity> optionalContent = contentRepository.findById(contentId);
+            if (optionalContent.isPresent()) {
+                // 콘텐츠가 존재하는 경우 수정
+                ContentEntity existingContent = optionalContent.get();
+                existingContent.setContentName(contentDTO.getContentName());
+                existingContent.setContentContent(contentDTO.getContentContent());
+
+                ContentEntity updatedContent = contentRepository.save(existingContent);
+                return ResponseEntity.ok(updatedContent);
+            } else {
+                // 콘텐츠가 없는 경우 404 Not Found 상태 반환
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            // 수정 도중 에러가 발생했을 때 500 Internal Server Error 상태와 함께 에러 메시지 반환
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
+
