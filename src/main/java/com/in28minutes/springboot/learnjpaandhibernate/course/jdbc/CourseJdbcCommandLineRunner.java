ce42ba8814c1da1a6faf3cdb8e4c3716b4b8d0f0
yeon-jae <lisanam99@naver.com>
2023-08-06 02:58:22 +0900

    jdbcCommand

---
+++ src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -0,0 +1,16 @@
+package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseJdbcCommandLineRunner implements CommandLineRunner {
+    @Autowired
+    private CourseJdbcRepository repository;
+    @Override
+    public void run(String... args) throws Exception {
+        //쿼리를 실행하게 됨
+        repository.insert();
+    }
+}
--- src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
+++ src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -1,4 +1,21 @@
 package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class CourseJdbcRepository {
+    @Autowired
+    private JdbcTemplate springJdbcTemplate;
+    //텍스트 블록 사용
+    private static  String INSERT_QUERY=
+            """
+                    insert into course(id,name,author)
+                    values(1,'Learn AWS','in28minutes');
+            """;
+    public void insert(){
+
+        springJdbcTemplate.update("INSERT_QUERY");
+    }
 }
