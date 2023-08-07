d8aaaa0309e8883ccdaba3ea7a2545c927b92cf7
yeon-jae <lisanam99@naver.com>
2023-08-07 14:48:15 +0900

    Helloworld_Rest_api

---
+++ src/main/java/com/springboot/rest/webservices/restfulwebservice/helloworld/HelloWorldBean.java
@@ -0,0 +1,24 @@
+package com.springboot.rest.webservices.restfulwebservice.helloworld;
+
+public class HelloWorldBean {
+    private String message;
+    public HelloWorldBean(String message) {
+        this.message=message;
+    }
+
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
