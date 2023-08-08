b1648b6042d0764727284a69de08127e81b23e19
yeon-jae <lisanam99@naver.com>
2023-08-08 12:39:59 +0900

    url_created

--- src/main/java/com/springboot/rest/webservices/restfulwebservice/user/UserDaoService.java
+++ src/main/java/com/springboot/rest/webservices/restfulwebservice/user/UserDaoService.java
@@ -26,6 +26,11 @@ public class UserDaoService {
         Predicate<? super User> predicate =user->user.getId().equals(id);
         return users.stream().filter(predicate).findFirst().orElse(null);
     }
+    //삭제 메소드
+    public void deleteById(int id){
+        Predicate<? super User> predicate =user->user.getId().equals(id);
+        users.removeIf(predicate);
+    }
 
     public User save(User user){
         user.setId(++userCount);
